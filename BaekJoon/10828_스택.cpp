c0a16271457644256032e520ab9d7990b3e6a59c
EunJin Lee <50702052+witheunjin@users.noreply.github.com>
2020-03-03 21:42:24 +0900

    BaekJoon

---
+++ BaekJoon/10828_스택.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+stack<int> s;
+void match(string para) {
+	if (para == "push") {
+		int x;
+		cin >> x;
+		s.push(x);
+	}
+	else if (para == "pop") {
+		if (s.empty()) cout << -1 << endl;
+		else { cout << s.top() << endl; s.pop(); }
+	}
+	else if (para == "size") {
+		cout << s.size() << endl;
+	}
+	else if (para == "empty") {
+		if (s.empty()) cout << 1 << endl;
+		else cout << 0 << endl;
+	}
+	else if (para == "top") {
+		if (s.empty()) cout << -1 << endl;
+		else cout << s.top() << endl;
+	}
+}
+int main() {
+	int N;
+	cin >> N;
+	string command;
+	for (int i = 0; i < N; ++i) {
+		cin >> command;
+		match(command);
+	}
+	return 0;
+}
